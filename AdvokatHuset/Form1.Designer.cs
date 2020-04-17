367d86364ecea060a723984ae5c3b8a99bc1a7af
Matfrejos123 <58071716+Matfrejos123@users.noreply.github.com>
2020-04-17 15:02:16 +0200

    Button -> KNAP !

--- AdvokatHuset/Form1.Designer.cs
+++ AdvokatHuset/Form1.Designer.cs
@@ -43,7 +43,7 @@
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "KNAP !";
             this.button1.UseVisualStyleBackColor = true;
             // 
             // textBox1
