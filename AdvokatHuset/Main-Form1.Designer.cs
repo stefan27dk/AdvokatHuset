d1d50811ab0d82784cab5303f38694292d7b0cf4
Stefan Biserov Popov <stef9633@edu.ucl.dk>
2020-04-19 21:51:18 +0200

    Main Form1 - med menu

---
+++ AdvokatHuset/Main-Form1.Designer.cs
@@ -0,0 +1,206 @@
+﻿namespace AdvokatHuset
+{
+    partial class Main_Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.opretToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.opretSagToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.opretAdvokatToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.opretSekretærToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.advokatToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.sekretærToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.visAlleMedarbejderToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.opretToolStripMenuItem,
+            this.advokatToolStripMenuItem,
+            this.sekretærToolStripMenuItem,
+            this.visAlleMedarbejderToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 8;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // opretToolStripMenuItem
+            // 
+            this.opretToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.opretSagToolStripMenuItem,
+            this.opretAdvokatToolStripMenuItem,
+            this.opretSekretærToolStripMenuItem});
+            this.opretToolStripMenuItem.Name = "opretToolStripMenuItem";
+            this.opretToolStripMenuItem.Size = new System.Drawing.Size(49, 20);
+            this.opretToolStripMenuItem.Text = "Opret";
+            // 
+            // opretSagToolStripMenuItem
+            // 
+            this.opretSagToolStripMenuItem.Name = "opretSagToolStripMenuItem";
+            this.opretSagToolStripMenuItem.Size = new System.Drawing.Size(153, 22);
+            this.opretSagToolStripMenuItem.Text = "Opret Sag";
+            // 
+            // opretAdvokatToolStripMenuItem
+            // 
+            this.opretAdvokatToolStripMenuItem.Name = "opretAdvokatToolStripMenuItem";
+            this.opretAdvokatToolStripMenuItem.Size = new System.Drawing.Size(153, 22);
+            this.opretAdvokatToolStripMenuItem.Text = "Opret Advokat";
+            // 
+            // opretSekretærToolStripMenuItem
+            // 
+            this.opretSekretærToolStripMenuItem.Name = "opretSekretærToolStripMenuItem";
+            this.opretSekretærToolStripMenuItem.Size = new System.Drawing.Size(153, 22);
+            this.opretSekretærToolStripMenuItem.Text = "Opret Sekretær";
+            // 
+            // advokatToolStripMenuItem
+            // 
+            this.advokatToolStripMenuItem.Name = "advokatToolStripMenuItem";
+            this.advokatToolStripMenuItem.Size = new System.Drawing.Size(73, 20);
+            this.advokatToolStripMenuItem.Text = "Advokater";
+            // 
+            // sekretærToolStripMenuItem
+            // 
+            this.sekretærToolStripMenuItem.Name = "sekretærToolStripMenuItem";
+            this.sekretærToolStripMenuItem.Size = new System.Drawing.Size(65, 20);
+            this.sekretærToolStripMenuItem.Text = "Sekretær";
+            // 
+            // visAlleMedarbejderToolStripMenuItem
+            // 
+            this.visAlleMedarbejderToolStripMenuItem.Name = "visAlleMedarbejderToolStripMenuItem";
+            this.visAlleMedarbejderToolStripMenuItem.Size = new System.Drawing.Size(125, 20);
+            this.visAlleMedarbejderToolStripMenuItem.Text = "Vis alle medarbejder";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(416, 286);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(40, 13);
+            this.label3.TabIndex = 15;
+            this.label3.Text = "Value3";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(416, 236);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 13);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "Value2";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(416, 192);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 13);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "Value1";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(479, 236);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 12;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(479, 280);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 11;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(479, 192);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 10;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(222, 150);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "KNAP !";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // Main_Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.menuStrip1);
+            this.Name = "Main_Form1";
+            this.Text = "Main_Form1";
+            this.Load += new System.EventHandler(this.Main_Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem opretToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem opretSagToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem opretAdvokatToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem opretSekretærToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem advokatToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem sekretærToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem visAlleMedarbejderToolStripMenuItem;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
--- AdvokatHuset/Main-Form1.cs
+++ AdvokatHuset/Main-Form1.cs
@@ -10,14 +10,14 @@ using System.Windows.Forms;
 
 namespace AdvokatHuset
 {
-    public partial class Form1 : Form
+    public partial class Main_Form1 : Form
     {
-        public Form1()
+        public Main_Form1()
         {
             InitializeComponent();
         }
 
-        private void Form1_Load(object sender, EventArgs e)
+        private void Main_Form1_Load(object sender, EventArgs e)
         {
 
         }
