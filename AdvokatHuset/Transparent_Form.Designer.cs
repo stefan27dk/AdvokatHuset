d651bf65d8039ce1f857d8fafaa3db696e4b23e9
Stefan Biserov Popov <stef9633@edu.ucl.dk>
2020-06-20 21:01:32 +0200

    Transparent Form As Panel - Update

--- AdvokatHuset/Transparent_Form.Designer.cs
+++ AdvokatHuset/Transparent_Form.Designer.cs
@@ -43,7 +43,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.pointer_hole_panel);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
